6e7c75063d98f5ce490a5c690cca90f7dba6f7d9
JEONG-BD <dev.byeongdu@gmail.com>
2025-06-23 10:22:47 +0900

    server, client 강제종료

---
+++ src/network/exception/close/reset/ResetCloseClient.java
@@ -0,0 +1,46 @@
+package network.exception.close.reset;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+
+import static util.MyLogger.log;
+
+public class ResetCloseClient {
+    public static void main(String[] args) throws InterruptedException, IOException {
+
+        Socket socket = new Socket("localhost" , 12345);
+        log("소켓 연결");
+        InputStream input = socket.getInputStream();
+        OutputStream output = socket.getOutputStream();
+
+        // client <- server : FIN
+        Thread.sleep(1000);
+
+        // client -> server PUSH
+        output.write(1);
+
+        // client <- server RST
+        Thread.sleep(1000);
+
+        try {
+
+            // java.net.SocketException: Connection reset 발생
+            int read = input.read();
+            System.out.println("read = " + read);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+
+            // java.net.SocketException: Boken pipe 발생
+            output.write(1);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+}
