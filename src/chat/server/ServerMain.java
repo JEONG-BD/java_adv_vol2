3c653349791dd17945e8f832a0793aa5edc7d174
JEONG-BD <dev.byeongdu@gmail.com>
2025-06-23 16:47:16 +0900

    Null Object DefaultCommand

--- src/chat/server/ServerMain.java
+++ src/chat/server/ServerMain.java
@@ -9,7 +9,9 @@ public class ServerMain {
     public static void main(String[] args) throws IOException {
         SessionManager sessionManager = new SessionManager();
         //CommandManagerV1 commandManager  = new CommandManagerV1(sessionManager);
-        CommandManagerV2 commandManager  = new CommandManagerV2(sessionManager);
+        //CommandManagerV2 commandManager  = new CommandManagerV2(sessionManager);
+        //CommandManagerV3 commandManager  = new CommandManagerV3(sessionManager);
+        CommandManagerV4 commandManager  = new CommandManagerV4(sessionManager);
 
         Server server = new Server(PORT, commandManager, sessionManager);
         server.start();
