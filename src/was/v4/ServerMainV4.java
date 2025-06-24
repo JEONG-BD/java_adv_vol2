599dacf15f4cb08bb0b4230a275b18e2738ebeca
JEONG-BD <dev.byeongdu@gmail.com>
2025-06-24 17:08:37 +0900

    handlerV4

---
+++ src/was/v4/ServerMainV4.java
@@ -0,0 +1,14 @@
+package was.v4;
+
+import was.v3.HttpServerV3;
+
+import java.io.IOException;
+
+public class ServerMainV4 {
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpServerV4 httpServerV4 = new HttpServerV4(PORT);
+        httpServerV4.start();
+    }
+}
