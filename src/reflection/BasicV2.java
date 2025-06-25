5a7ca479456465a199351f388e87eae3548e2c96
JEONG-BD <dev.byeongdu@gmail.com>
2025-06-25 09:11:09 +0900

    reflection class meta

---
+++ src/reflection/BasicV2.java
@@ -0,0 +1,24 @@
+package reflection;
+
+import reflection.data.BasicData;
+
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+public class BasicV2 {
+    public static void main(String[] args) {
+        Class<BasicData> basicData = BasicData.class;
+        System.out.println("basicData.getName() = " + basicData.getName());
+        System.out.println("basicData.getSimpleName() = " + basicData.getSimpleName());
+        System.out.println("basicData.getPackage() = " + basicData.getPackage());
+
+        System.out.println("basicData.getSuperclass() = " + basicData.getSuperclass());
+        System.out.println("basicData.getInterfaces() = " + Arrays.toString(basicData.getInterfaces()));
+        System.out.println("basicData.isInstance() = " + basicData.isInterface());
+
+        int modifiers = basicData.getModifiers();
+        System.out.println("modifiers = " + modifiers);
+        boolean isPublic = Modifier.isPublic(modifiers);
+        System.out.println("isPublic = " + isPublic);
+    }
+}
